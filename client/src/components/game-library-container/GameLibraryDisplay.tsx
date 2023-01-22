9e271ec1444973d559d05598bd6e9bcbfd559557
ahasan02 <ahasan02@student.ubc.ca>
2023-01-22 09:41:53 -0800

    added instructions to games

--- client/src/components/game-library-container/GameLibraryDisplay.tsx
+++ client/src/components/game-library-container/GameLibraryDisplay.tsx
@@ -6,19 +6,19 @@ const GameLibraryDisplay = () => {
     <div className="LibraryMainDisplay">
       <GameLibraryEntry
         title="Necromancy"
-        description="There's a necromancer at your school, take out his neverending legion of undead warriors!"
+        description="There's a necromancer at your school, take out his neverending legion of undead warriors! Arrow keys to aim, spacebar to shoot."
         splashLink="https://i.imgur.com/zVWUoOV.png"
         gameLink="http://localhost:8000/public/necromancy"
       />
       <GameLibraryEntry
         title="Space Commander"
-        description="Escape from the unrelenting alien fleet!"
+        description="Escape from the unrelenting alien fleet! Arrow keys to move, spacebar to shoot."
         splashLink="https://i.imgur.com/z8qipfA.png"
         gameLink="http://localhost:8000/public/platy"
       />
       <GameLibraryEntry
         title="Bong"
-        description="Bong => Bad Pong. You never win. Don't even try."
+        description="Bong => Bad Pong. You never win. Don't even try. Up/Down to move."
         splashLink="https://i.imgur.com/a1CxN0t.png"
         gameLink="http://localhost:8000/public/bong"
       />
